f25692ba3870914dfae3d911d13c49230715c02f
Henric Lindén <henric.linden@gmail.com>
2017-08-29 21:50:57 +0200

    Generating icons for iPhone and iPad

--- generate_appicons.sh
+++ generate_appicons.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# set -x
+
 E_NOARGS=85
 E_NOTFOUND=86
 
@@ -23,5 +25,13 @@ do
 	sips -Z $((i*2)) --out ${filename%.*}-${i}x${i}@2x.png $filename
 done
 
+for i in {20,29,40,60,76}
+do
+	sips -Z $i --out ${filename%.*}-${i}x${i}.png $filename
+	sips -Z $((i*2)) --out ${filename%.*}-${i}x${i}@2x.png $filename
+	sips -Z $((i*3)) --out ${filename%.*}-${i}x${i}@3x.png $filename
+done
+
+sips -Z 167 --out ${filename%.*}-83.5x83.5@2x.png $filename
 
 
